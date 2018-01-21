30bc9e627df305b6a5bd10af433c39669d130ebb
Adam Bloss <adam.bloss17@gmail.com>
2018-01-21 16:28:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes = {}
+function updateObjectWithKeyAndValue(object, key, value) {
+  var obj = object.assign({key:value},object);
+  return obj
+}
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  return object.key = value;
+}
+function deleteFromObjectByKey(object, key) {
+  var obj = delete object.key;
+  return obj
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+}
